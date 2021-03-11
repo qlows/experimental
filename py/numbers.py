aef77e36cba7271f74fe12950187d4cf9626602b
qlows <kilincumitjr@gmail.com>
2021-03-10 19:35:40 -0500

    py

---
+++ py/numbers.py
@@ -0,0 +1,51 @@
+# Python Numbers
+
+x = 1
+y = 2.8
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#int
+x = 1
+y = 355569741232985
+z = -9875584
+
+
+#float
+x = 1.10
+y = 3.0
+z = -65.58
+a = 35e85
+k = 58E1
+l = -5.2e1
+
+#complex
+x = 3+5j
+y = 5j
+z = -6j
+
+
+#convert from in to float
+x = float(1)
+
+#convert from float to int
+y = int(20.8)
+
+#convert from int to complex
+z = complex(x)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+import random
+print(random.randrange(1,25))
--- py/py-data-types.py
+++ py/py-data-types.py
@@ -102,4 +102,3 @@ x = memoryview(bytes(5))
 print(x)
 print(type(x))
 
-
