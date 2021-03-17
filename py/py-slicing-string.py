70c9101532b45ac6bd5bb9fa432e62728e32ad0b
qlows <kilincumitjr@gmail.com>
2021-03-17 18:08:09 -0400

    qqw

---
+++ py/py-slicing-string.py
@@ -0,0 +1,15 @@
+#the first character has index 0.
+
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+b = "Hello, World!"
+print(b[2:])
+
+
+b = "Hello, World!"
+print(b[-5:-2])
+
