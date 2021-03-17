f63391050fb89cf095a73fbe5cd7889a30c1aae8
qlows <kilincumitjr@gmail.com>
2021-03-17 18:17:06 -0400

    i

---
+++ py/py-modify-strings.py
@@ -0,0 +1,24 @@
+#Upper case
+
+a = "Hello, World!"
+print(a.upper())
+
+#Lower case
+
+a = "Hello, World!"
+print(a.lower())
+
+#Remove whitespace
+a = " Hello, World! "
+print(a.strip())
+
+#Replace string
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+#Split string
+
+a = "Hello, World!"
+b = a.split(",")
+print(b)
