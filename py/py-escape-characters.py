d4d4fecf535580eb771b92fb50d1c5bbb4cdca12
qlows <kilincumitjr@gmail.com>
2021-03-17 19:54:48 -0400

    n

---
+++ py/py-escape-characters.py
@@ -0,0 +1,34 @@
+txt = "We are the so-called \"Vikings\" fromt the north."
+print(txt)
+
+#single quote
+txt = "It\'s alright."
+print(txt)
+
+#backslash
+txt = "This will insert one \\ (blackslash)"
+print(txt)
+
+#new line
+txt = "Hello\nWorld!"
+print(txt)
+
+#Carriage return
+txt = "Well\rBeing"
+print(txt)
+
+#tab
+txt = "Whats\tup?"
+print(txt)
+
+#backspace (it erases one character)
+txt = "Hello \bWorld"
+print(txt)
+
+#octal value (a backslash followed by three integers will result in a octal value:)
+txt = "\110\145\154\154\157"
+print(txt)
+
+#hex value (a backslah followed by an 'x' and a hex number represents a hex value:)
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt)
