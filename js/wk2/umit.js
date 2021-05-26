eab8664c855f2a5274b3f5e1e3441f7b34ac50fa
qlows <79415956+qlows@users.noreply.github.com>
2021-05-26 18:57:13 -0400

    Add files via upload

---
+++ js/wk2/umit.js
@@ -0,0 +1,15 @@
+/* "use strict";
+
+let topSirloin = parseInt(prompt("How many pounds of top sirloin would you like to order?"));
+
+document.write(`<p>You ordered ${topSirloin} lbs of Top Sirloin </p>`);
+var sirloinPrice = 5.12;
+const tax = 0.13
+var total = (sirloinPrice + topSirloin * tax) + (topSirloin * sirloinPrice)
+document.write(`<p>The subtotal is: $${(topSirloin * sirloinPrice)}</p>`);
+document.write(`<p>The total is: $${total.toFixed(2)}</p>`);
+
+
+ */
+
+var a = 3; if(a == "b"){ a = a * 2; }
